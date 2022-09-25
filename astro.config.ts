a373703a3b11857d8e91005bece9244471f7e351
Adam Collier <adam@logicandrhythm.com>
2022-09-25 21:11:43 +0100

    prefectch, image, svelte,

--- astro.config.ts
+++ astro.config.ts
@@ -1,18 +1,26 @@
 import { defineConfig } from 'astro/config'
-import vercelAdapter from '@astrojs/vercel/serverless' // @ts-ignore: this will be fixed in the next unocss release
-
+import vercelAdapter from '@astrojs/vercel/serverless'
+import unocss from 'unocss/astro'
 import preact from '@astrojs/preact'
-
 import { typography } from './src/utils/rehype/typography'
 import { highlight } from './src/utils/rehype/highlight'
+import prefetch from '@astrojs/prefetch'
+import image from '@astrojs/image'
+
+import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercelAdapter(),
-  integrations: [preact()],
+  integrations: [unocss(), preact(), prefetch(), image(), svelte()],
   markdown: {
     syntaxHighlight: false,
     rehypePlugins: [highlight, typography],
+  },
+  vite: {
+    ssr: {
+      external: ['image-size', 'tiny-glob'],
+    },
   },
 })
